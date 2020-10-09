39d9922c57bda60a3450ff51dbe2d38d7cd60367
Kcnarf <fl.franck.lebeau@gmail.com>
2020-10-09 15:48:18 +0200

    [spec] spec for data mapping thanks to starting/ending keys

--- test/d3-voronoi-map-tween-test.js
+++ test/d3-voronoi-map-tween-test.js
@@ -3,9 +3,9 @@ const tape = require('tape'),
   d3VoronoiMapTween = require('../build/d3-voronoi-map-tween');
 
 tape('voronoiMapTween should provide tween types', function (test) {
-  test.ok(voronoiMapTween.ENTER_TWEEN_TYPE);
-  test.ok(voronoiMapTween.UPDATE_TWEEN_TYPE);
-  test.ok(voronoiMapTween.EXIT_TWEEN_TYPE);
+  test.ok(d3VoronoiMapTween.ENTER_TWEEN_TYPE);
+  test.ok(d3VoronoiMapTween.UPDATE_TWEEN_TYPE);
+  test.ok(d3VoronoiMapTween.EXIT_TWEEN_TYPE);
   test.end();
 });
 
@@ -66,33 +66,86 @@ tape('voronoiMapTween.endingKey should set specified identifier accessor', funct
   test.end();
 });
 
-tape('voronoiMapTween(...) should idenditify added/deleted/updated data', function (test) {
-  const deletedData = { id: 0, weight: 1 },
-    updatedData = { id: 1, weight: 1 },
-    addedData = { id: 2, weight: 1 },
-    voronoiMapSimulation0 = d3VoronoiMap.voronoiMapSimulation([deletedData, updatedData]).stop(),
-    voronoiMapSimulation1 = d3VoronoiMap.voronoiMapSimulation([updatedData, addedData]).stop();
-  const voronoiMapTween = d3VoronoiMapTween.voronoiMapTween(voronoiMapSimulation0, voronoiMapSimulation1);
+tape('voronoiMapTween(...)', function (test) {
+  tape('voronoiMapTween(...) should idenditify added/entered, exited/deleted, and updated data', function (test) {
+    const deletedData = { id: 0, weight: 1 },
+      updatedData = { id: 1, weight: 1 },
+      addedData = { id: 2, weight: 1 },
+      voronoiMapSimulation0 = d3VoronoiMap.voronoiMapSimulation([deletedData, updatedData]).stop(),
+      voronoiMapSimulation1 = d3VoronoiMap.voronoiMapSimulation([updatedData, addedData]).stop();
+    const voronoiMapTween = d3VoronoiMapTween.voronoiMapTween(voronoiMapSimulation0, voronoiMapSimulation1);
 
-  const interpolatedVoronoiMapAt0 = voronoiMapTween(0);
-  const polygonOfDeletedDataAt0 = interpolatedVoronoiMapAt0.find(function (p) {
-    return p.site.originalObject.startingData === deletedData;
-  });
-  const polygonOfUpdatedDataAt0 = interpolatedVoronoiMapAt0.find(function (p) {
-    return p.site.originalObject.startingData === updatedData;
-  });
-  test.equal(polygonOfDeletedDataAt0.site.originalObject.tweenType, d3VoronoiMapTween.EXIT_TWEEN_TYPE);
-  test.equal(polygonOfUpdatedDataAt0.site.originalObject.tweenType, d3VoronoiMapTween.UPDATE_TWEEN_TYPE);
+    const interpolatedVoronoiMapAt0 = voronoiMapTween(0);
+    const polygonOfDeletedDataAt0 = interpolatedVoronoiMapAt0.find(function (p) {
+      return p.site.originalObject.startingData === deletedData;
+    });
+    const polygonOfUpdatedDataAt0 = interpolatedVoronoiMapAt0.find(function (p) {
+      return p.site.originalObject.startingData === updatedData;
+    });
+    test.equal(polygonOfDeletedDataAt0.site.originalObject.tweenType, d3VoronoiMapTween.EXIT_TWEEN_TYPE);
+    test.equal(polygonOfUpdatedDataAt0.site.originalObject.tweenType, d3VoronoiMapTween.UPDATE_TWEEN_TYPE);
 
-  const interpolatedVoronoiMapAt1 = voronoiMapTween(1);
-  const polygonOfUpdatedDataAt1 = interpolatedVoronoiMapAt1.find(function (p) {
-    return p.site.originalObject.endingData === updatedData;
-  });
-  const polygonOfAddedDataAt1 = interpolatedVoronoiMapAt1.find(function (p) {
-    return p.site.originalObject.endingData === addedData;
-  });
-  test.equal(polygonOfUpdatedDataAt1.site.originalObject.tweenType, d3VoronoiMapTween.UPDATE_TWEEN_TYPE);
-  test.equal(polygonOfAddedDataAt1.site.originalObject.tweenType, d3VoronoiMapTween.ENTER_TWEEN_TYPE);
+    const interpolatedVoronoiMapAt1 = voronoiMapTween(1);
+    const polygonOfUpdatedDataAt1 = interpolatedVoronoiMapAt1.find(function (p) {
+      return p.site.originalObject.endingData === updatedData;
+    });
+    const polygonOfAddedDataAt1 = interpolatedVoronoiMapAt1.find(function (p) {
+      return p.site.originalObject.endingData === addedData;
+    });
+    test.equal(polygonOfUpdatedDataAt1.site.originalObject.tweenType, d3VoronoiMapTween.UPDATE_TWEEN_TYPE);
+    test.equal(polygonOfAddedDataAt1.site.originalObject.tweenType, d3VoronoiMapTween.ENTER_TWEEN_TYPE);
 
+    test.end();
+  });
+
+  tape('voronoiMapTween(...) should correctly map data thanks to starting/ending keys', function (test) {
+    const dataOnlyAtStart = { identifier: 0, weight: 1 },
+      data1AtStart = { identifier: 1, weight: 1 },
+      data2AtStart = { identifier: 2, weight: 1 },
+      data1AtEnd = { idPrime: 1, weight: 1 },
+      data2AtEnd = { idPrime: 2, weight: 1 },
+      dataOnlyAtEnd = { identifier: 3, weight: 1 },
+      startingKey = function (d) {
+        return d.identifier;
+      },
+      endingKey = function (d) {
+        return d.idPrime;
+      },
+      voronoiMapSimulation0 = d3VoronoiMap.voronoiMapSimulation([dataOnlyAtStart, data1AtStart, data2AtStart]).stop(),
+      voronoiMapSimulation1 = d3VoronoiMap.voronoiMapSimulation([data1AtEnd, data2AtEnd, dataOnlyAtEnd]).stop();
+    const voronoiMapTween = d3VoronoiMapTween
+      .voronoiMapTween(voronoiMapSimulation0, voronoiMapSimulation1)
+      .startingKey(startingKey)
+      .endingKey(endingKey);
+
+    let interpolatedVoronoiMap = voronoiMapTween(0);
+    const interpolatedPolygonOfDataOnlyAtStart = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.startingData === dataOnlyAtStart;
+    });
+    let interpolatedPolygonOfData1 = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.startingData === data1AtStart;
+    });
+    let interpolatedPolygonOfData2 = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.startingData === data2AtStart;
+    });
+    test.equal(interpolatedPolygonOfDataOnlyAtStart.site.originalObject.endingData, null); // no mapping, added/entering data
+    test.equal(interpolatedPolygonOfData1.site.originalObject.endingData, data1AtEnd);
+    test.equal(interpolatedPolygonOfData2.site.originalObject.endingData, data2AtEnd);
+
+    interpolatedVoronoiMap = voronoiMapTween(1);
+    interpolatedPolygonOfData1 = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.endingData === data1AtEnd;
+    });
+    interpolatedPolygonOfData2 = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.endingData === data2AtEnd;
+    });
+    const interpolatedPolygonOfDataOnlyAtEnd = interpolatedVoronoiMap.find(function (p) {
+      return p.site.originalObject.endingData === dataOnlyAtEnd;
+    });
+    test.equal(interpolatedPolygonOfData1.site.originalObject.startingData, data1AtStart);
+    test.equal(interpolatedPolygonOfData2.site.originalObject.startingData, data2AtStart);
+    test.equal(interpolatedPolygonOfDataOnlyAtEnd.site.originalObject.startingData, null); // no mapping, deleted/exiting data
+    test.end();
+  });
   test.end();
 });
